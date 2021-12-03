81a699bbd250872822801ea28279b8cbc3fbb4b5
Paulo <paulosenscoelho@gmail.com>
2021-12-03 17:06:19 -0300

    testando orient a objs no algoritmo de economias

--- project/EvrpSolution.h
+++ project/EvrpSolution.h
@@ -1,103 +1,22 @@
-class EvrpData {
-public:
-	EvrpData(string instance);
-    int pointsSize();
-    vector<double> demands();
-    double loadCapacity();
-    vector<vector<double>> distMatrix();
-    vector<vector<double>> energyMatrix();
-    
-
-	vector<pair<double, double>> _points;
-    vector<double> _demands;
-    double _loadCapacity;
-	vector<vector<double>> _distMatrix;
-	vector<vector<double>> _energyMatrix;
-}
-
-EvrpData::EvrpData(string instance) {
-    string myText;
-	ifstream MyReadFile(instance);
-    // Pulando a linha de cabeçalho.
-    getline(MyReadFile, myText);
-    
-    // Laço de leitura dos dados do arquivo.
-    while(getline(MyReadFile, myText)){
-        // Usado para dividir as strings nos espaços vazios.
-        istringstream ss(myText);
-        string word, type, x, y;   
-        
-        ss >> word;
-        ss >> type;
-        ss >> x; 
-        ss >> y;
-        if (type == "d" or type == "c") // or type == "f")
-        {            
-            pair <double, double> pointPair (stod(x), stod(y));
-            _points.push_back(pointPair);
-            
-            string pointDemand;
-            ss >> pointDemand; 
-            _demands.push_back(stod(pointDemand));
-        }
-        else if (word == "C")
-        {
-            string loadCapacityStr;
-            ss >> loadCapacityStr;
-            loadCapacityStr.erase(0, 1);
-            loadCapacityStr.pop_back();
-            _loadCapacity = stod(loadCapacityStr);
-        }
-    }
-    MyReadFile.close();
-
-	int sizePoints = _points.size();
-    int lastPoint = sizePoints - 1;
-	vector<vector<double>> auxMatrix(sizePoints, vector<double>(sizePoints, 0.0));
-
-	_distMatrix = auxMatrix;
-	_energyMatrix = auxMatrix;
-
-    // Preenchimento da matriz de distâncias e de energias.
-    for (int i = 0; i < sizePoints; i++)
-    {
-        for (int j = 0; j < sizePoints; j++)
-        {
-            _distMatrix[i][j] = distancePoints(_points[i], _points[j]);
-            _energyMatrix[i][j] = ENERGY_CONST * _distMatrix[i][j];
-        }
-    }
-}
-
-vector<double> EvrpData::demands(){
-    return _demands;
-}
-
-vector<vector<double>> EvrpData::energyMatrix(){
-    return _energyMatrix;
-}
-
-vector<vector<double>> EvrpData::distMatrix(){
-    return _distMatrix;
-}
-
-double EvrpData::loadCapacity(){
-    return _loadCapacity;
-}
-
-int EvrpData::pointsSize() {
-    return _points.size();
-}
-
+#include "EvrpUtils.h"
 class EvrpSolution {
 public:
     EvrpData* _evrpData;
     vector<vector<int>> _routes;
     vector<double> _routesEnergy;
+    vector<vector<int>> routes();
+    vector<double> routesEnergy();
     EvrpSolution(EvrpData evrpDataParameter);
     savingsAlg();
-    insertDepoAndEnergies();
-    
+    insertDepoAndEnergies();   
+}
+
+vector<vector<int>> EvrpSolution::routes() {
+    return _routes;
+}
+
+vector<double> EvrpSolution::routesEnergy() {
+    return _routesEnergy;
 }
 
 EvrpSolution::EvrpSolution(EvrpData* evrpDataParameter) {
@@ -111,13 +30,13 @@ EvrpSolution::EvrpSolution(EvrpData* evrpDataParameter) {
 
 EvrpSolution::savingsAlg() {
     priority_queue<saving> savingQueue;
-    vector<vector<double>> distMatrix = _evrpData.distMatrix();
-    vector<double> demands = _evrpData.demands()
+    vector<vector<double>> distMatrix = _evrpData->distMatrix();
+    vector<double> demands = _evrpData->demands()
 
     // Cálculo das economias das arestas
-    for (int i = 1; i < _evrpData.pointsSize() - 1; i++)
+    for (int i = 1; i < _evrpData->pointsSize() - 1; i++)
     {
-        for (int j = i + 1; j <= _evrpData.pointsSize() - 1; j++)
+        for (int j = i + 1; j <= _evrpData->pointsSize() - 1; j++)
         {
             double savingIJ = distMatrix[0][i] + distMatrix[0][j] - distMatrix[i][j];
             struct saving savingStruct;
@@ -199,9 +118,9 @@ EvrpSolution::savingsAlg() {
 
 EvrpSolution::insertDepoAndEnergies() {
     vector<double> routesEnergy;
-    vector<double> demands = _evrpData.demands()
-    vector<vector<double>> distMatrix = _evrpData.distMatrix();
-    vector<vector<double>> energyMatrix = _evrpData.energyMatrix();
+    vector<double> demands = _evrpData->demands();
+    vector<vector<double>> distMatrix = _evrpData->distMatrix();
+    vector<vector<double>> energyMatrix = _evrpData->energyMatrix();
 
     for (int i = 0; i < _routes.size(); i++)
     {
---
+++ project/evrp.cpp
@@ -0,0 +1,16 @@
+#include "EvrpData.h"
+#include "EvrpSolution.h"
+using namespace std;
+
+int main(int argc, char** argv){
+
+    EvrpData evrpDataobj = EvrpData(argv[1]);
+    EvrpSolution evrpSolution = EvrpSolution(&evrpDataobj);
+    evrpSolution.insertDepoAndEnergies();
+    string strSolucao = "/Users/paulo/Desktop/evrp/evrptw_instances/soluções/solução";
+    strSolucao = strSolucao + argv[1];
+    
+    writeSolutionSavings(evrpSolution.routes(), evrpSolution.routesEnergy(), strSolucao);
+
+	return 0;
+}
