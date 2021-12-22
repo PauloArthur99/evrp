51558fa66df4140c246e53b8020eedd608d0b99d
Paulo <paulosenscoelho@gmail.com>
2021-12-22 19:52:55 -0300

    2-opt firstfit  funcionando

--- evrp.cpp
+++ evrp.cpp
@@ -8,11 +8,11 @@ int main(int argc, char** argv){
     EvrpData evrpDataobj = EvrpData(argv[1]);
     EvrpSolution evrpSolution = EvrpSolution(&evrpDataobj);
     evrpSolution.savingsAlg();
+    hillClimbingEachRoute(&evrpSolution, 0);
+    
     evrpSolution.insertDepoAndEnergies();
     string strSolucao = "/Users/paulo/Desktop/evrp/evrptw_instances/soluções/solução";
     strSolucao = strSolucao + argv[1];
-    hillClimbingEachRoute(&evrpSolution, 0);
-    
 
     writeSolutionSavings(evrpSolution.routes(), evrpSolution.routesEnergy(), strSolucao);
 
